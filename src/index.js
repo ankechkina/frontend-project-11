f5ec98be452098f2496f6069bec6d270403ec212
Nastya <kechnastya@gmail.com>
2024-03-12 14:20:12 +0300

    add rss validation

--- src/index.js
+++ src/index.js
@@ -9,55 +9,93 @@ import axios from 'axios';
 const state = {
   currentInput: '',
   isValid: true,
-  errors: [],
+  currentError: '',
   rssFeeds: [],
 };
 
 const getPath = (url) => `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`;
 const validUrlSchema = yup.string().url('Неправильный формат URL');
 
+const isValidRss = (site) => site.includes('<rss') || site.includes('<channel');
+
 const form = document.querySelector('form');
 const urlInput = document.querySelector('#url-input');
-const errorMessageField = document.querySelector('.text-danger');
+const feedbackMessage = document.querySelector('.feedback');
+const submitButton = document.querySelector('button[type="submit"]');
 
 const render = () => {
-  if (state.isValid === false) {
+  // console.log('state changed!');
+  // сначала убираем оба класса, чтобы наверняка, а потом уже добавляем нужные
+  urlInput.classList.remove('is-invalid');
+  feedbackMessage.classList.remove('text-danger');
+  feedbackMessage.classList.remove('text-success');
+  feedbackMessage.textContent = '';
+
+  if (!state.isValid) {
     urlInput.classList.add('is-invalid');
-    errorMessageField.textContent = state.errors[0];
-  } else if (state.isValid === true) {
-    urlInput.classList.remove('is-invalid');
-    errorMessageField.textContent = '';
+    feedbackMessage.classList.add('text-danger');
+    feedbackMessage.textContent = state.currentError;
+  } else if (state.isValid && state.rssFeeds.length !== 0) {
+    // если состояние валидно и массив rss feeds не пустой, то все прошло успешно
+    feedbackMessage.classList.add('text-success');
+    feedbackMessage.textContent = 'RSS успешно загружен';
   }
+  submitButton.disabled = false;
 };
 
 const watchedState = onChange(state, render);
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  submitButton.disabled = true;
+
+  // очищаем ошибки в state перед каждым сценарием, чтобы watchedState мог измениться
+  state.isValid = true;
+  state.currentError = '';
+
   const formData = new FormData(e.target);
   const inputValue = formData.get('url');
-  state.currentInput = inputValue;
+  const normalizedInput = inputValue.toLowerCase().trim();
+
+  state.currentInput = normalizedInput;
   form.reset();
 
-  validUrlSchema.isValid(inputValue)
+  validUrlSchema.isValid(state.currentInput)
     .then((isValid) => {
-      console.log(`${inputValue} - ${isValid ? 'Валидный URL' : 'Невалидный URL'}`);
-      if (!isValid && state.errors.length === 0) {
-        state.errors.push('Невалидный URL');
+      if (!isValid) {
+        state.currentError = 'Невалидный адрес URL';
         watchedState.isValid = false;
       }
       if (isValid) {
-        state.errors = [];
-        watchedState.isValid = true;
+        const path = getPath(state.currentInput);
+
+        fetch(path)
+          .then((response) => {
+            if (response.ok) return response.json();
+            watchedState.currentError = 'Проблемы с ответом от сервера';
+            return Promise.reject();
+            // throw new Error('Network response was not ok.');
+          })
+          .then((data) => {
+            const pageContent = data.contents;
+            const validRssFeed = isValidRss(pageContent);
+
+            if (!validRssFeed) {
+              state.currentError = 'Невалидный адрес RSS';
+              watchedState.isValid = false;
+            } else if (validRssFeed && !state.rssFeeds.includes(state.currentInput)) {
+              state.currentError = '';
+              state.isValid = true;
+              watchedState.rssFeeds.push(state.currentInput);
+            } else if (validRssFeed && state.rssFeeds.includes(state.currentInput)) {
+              state.currentError = 'RSS уже существует';
+              watchedState.isValid = false;
+            }
+          });
       }
     })
     .catch((error) => {
-      console.error(`Ошибка при проверке URL: ${error.message}`);
+      state.currentError = `Ошибка при проверке URL: ${error.message}`;
+      watchedState.isValid = false;
     });
-
-  console.log(state);
-  const path = getPath(inputValue);
 });
-
-// будем добавлять в состояние только валидный rss поток
-//
